921488466d2a1f5490f57782b542bde4e424897b
Martin <martintlolka@gmail.com>
2016-04-15 21:09:57 +0200

    Tutorial finished

--- src/ReactTutorial/Startup.cs
+++ src/ReactTutorial/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using React.AspNet;
+using Microsoft.AspNet.StaticFiles;
 
 namespace ReactTutorial
 {
@@ -15,6 +17,8 @@ namespace ReactTutorial
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddReact(); // Add React to the IoC container
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,9 +26,26 @@ namespace ReactTutorial
         {
             app.UseIISPlatformHandler();
 
-            app.Run(async (context) =>
+            // Initialise ReactJS.NET. Must be before static files.
+            app.UseReact(config =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                config
+                    .SetReuseJavaScriptEngines(true)
+                    .AddScriptWithoutTransform("~/build/server.bundle.js");
+            });
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                ServeUnknownFileTypes = true
+            });
+
+            app.UseMvc(r =>
+            {
+                r.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" }
+                );
             });
         }
 
---
+++ src/ReactTutorial/app/HelloWorld.jsx
@@ -0,0 +1,9 @@
+"use strict";
+/// <reference path="../../../typings/main/ambient/react/index.d.ts" />
+const React = require('react');
+class HelloMessage extends React.Component {
+    render() {
+        return <div>Hello {this.props.name}</div>;
+    }
+}
+module.exports = HelloMessage;
